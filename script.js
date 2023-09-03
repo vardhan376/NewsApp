3e0c3102dc489d15bb10b55a1815781c4358a833
HARSH <hv21csb0a21@student.nitw.ac.in>
2023-09-04 04:11:51 +0530

    Api key

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_KEY = "1d3a0eefa97b499d8fbc4ee93eeb40b7";
+const API_KEY = "b893aa24f3d94305b96d29fc964740f3";
 const url = "https://newsapi.org/v2/everything?q=";
 
 window.addEventListener("load", () => fetchNews("India"));
